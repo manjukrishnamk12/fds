497aacf61bb3d538406246c0952b1d486c406b08
manjukrishnamk12 <manjukrishnamk12@gmail.com>
2023-08-03 20:16:11 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,89 @@
+// console.log('Welcome to javascript');
+// var a=2;
+// console.log(a);
+// var x=10;
+// var y=22;
+// var result=x/y;
+// var r=x%y;
+// console.log(result);
+// console.log(r);
+
+///    OBJECTS
+
+//---------------object literal----------------
+
+// let person={
+//     eyeColor:'black',
+//     age:25,
+//     height:165
+// }
+// console.log(person);
+// console.log(person.eyeColor,person.age,person.height);
+
+
+// function operations(a,b) {
+//     var add=a+b;
+//     var sub=a-b;
+//     console.log(add,sub);
+// }
+// operations(5,4);
+
+//-------------object inside object--------------
+
+// let person={
+//     address:{
+//         strret:'kadappoor',
+//         city:'kottayam',
+//         state:'kerala'
+//     }
+// }
+// console.log(person);
+// console.log(person.address.city);
+
+
+//----------------------object using new keyword--------------------
+
+// let car= new Object();
+// car.model='hyundai';
+// car.color='black';
+// console.log(car);
+// console.log(car.color);
+
+//-----ARRAY-----
+
+// var arr1=['blue','red','orange']
+// console.log(arr1);
+// console.log(arr1[1]);
+// arr1.push('green');
+// console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+// console.log(arr1.length);
+// console.log(arr1.indexOf('red'));
+
+//---------ARRAY OF OBJECTS-----------
+
+// var arr=[{name:'hello',age:20},{name:'leela',age:30}]
+// console.log(arr[1].name);//to print the name leela
+
+//---------FUNCTION---------------
+
+// function display(a) {
+//     console.log(a);
+// }
+// display('manju');
+
+//---------------objects methods----------------
+
+// var person={
+//     fName:'jaya',
+//     lName:'sree',
+//     fullName:function(){
+//         return this.fName + ' ' + this.lName;
+//     }
+// }
+// var name=person.fullName();
+// console.log(name);
+
+//-----------------LOOPS-------------
+
---
+++ loops.js
@@ -0,0 +1,53 @@
+//print 1 to 10 numbers
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);   
+// }
+
+//print array elements
+var x=['mango','grape','coconut'];
+// for (let i = 0; i <x.length; i++) 
+// {
+//     console.log(x[i]);  
+// }
+
+//------for in loop
+// for(let i in x){
+//     console.log(x[i]);
+// }
+
+//------for of loop
+// for(let i of x){
+//     console.log(x[i]);
+// }
+//while loop
+// let i=1;
+// while (i<=10) {
+//     console.log(i);
+// }
+
+//using array
+
+// let i=0;
+// while (i<x.length) {
+//     console.log(x[i]);
+//     i++
+// }
+
+//----------DO WHILE---------
+// let i=1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<=10);
+
+let i=0;
+do {
+    console.log(x[i]);
+    i++;
+} while (i<x.length);
+
+
+
+
+
+
